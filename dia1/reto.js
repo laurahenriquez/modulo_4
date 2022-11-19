c111b5145e4e4e2778418fb6c4f6cb311b65a9cc
laurahenriquez <laurahenriquezcor@gmail.com>
2022-11-19 12:14:34 +0100

    reto2

--- dia1/reto.js
+++ dia1/reto.js
@@ -18,94 +18,116 @@ connection.connect((error) => {
 
 let sql = ""
 
-sql = "CREATE TABLE `dia1`.`students` (`student_id` INT NOT NULL AUTO_INCREMENT,`first_name` VARCHAR(45) NULL"
-+ ",`last_name` VARCHAR(45) NULL,`group_id` VARCHAR(45) NULL,PRIMARY KEY (`student_id`))";
+// sql = "CREATE TABLE `dia1`.`students` (`student_id` INT NOT NULL AUTO_INCREMENT,`first_name` VARCHAR(45) NULL"
+// + ",`last_name` VARCHAR(45) NULL,`group_id` VARCHAR(45) NULL,PRIMARY KEY (`student_id`))";
 
-connection.query(sql, function(err,result){
+// connection.query(sql, function(err,result){
+//     if(err){
+//         console.log(err);
+//     }else{
+//         console.log("Tabla creada");
+//         console.log(result);
+//     }
+// });
+
+// sql = "CREATE TABLE `dia1`.`subjects` (`subject_id` INT NOT NULL AUTO_INCREMENT,`title` VARCHAR(45) NULL,PRIMARY KEY (`subject_id`))";
+
+// connection.query(sql, function(err,result){
+//     if(err){
+//         console.log(err);
+//     }else{
+//         console.log("Tabla creada");
+//         console.log(result);
+//     }
+// });
+
+// sql = "CREATE TABLE `dia1`.`teachers` (`teacher_id` INT NOT NULL AUTO_INCREMENT,`first_name` VARCHAR(45) NULL,`last_name` VARCHAR(45) NULL,PRIMARY KEY (`teacher_id`))";
+
+// connection.query(sql, function(err,result){
+//     if(err){
+//         console.log(err);
+//     }else{
+//         console.log("Tabla creada");
+//         console.log(result);
+//     }
+// });
+
+// sql = "CREATE TABLE `dia1`.`subject_teacher` (`subject_id` INT NOT NULL AUTO_INCREMENT,`teacher_id` INT NOT NULL,`group_id` INT NOT NULL,PRIMARY KEY (`subject_id`))";
+
+// connection.query(sql, function(err,result){
+//     if(err){
+//         console.log(err);
+//     }else{
+//         console.log("Tabla creada");
+//         console.log(result);
+//     }
+// });
+
+// sql = "CREATE TABLE `dia1`.`groups` (`group_id` INT NOT NULL AUTO_INCREMENT,`name` INT NOT NULL,PRIMARY KEY (`group_id`))";
+
+// connection.query(sql, function(err,result){
+//     if(err){
+//         console.log(err);
+//     }else{
+//         console.log("Tabla creada");
+//         console.log(result);
+//     }
+// });
+
+// sql = "CREATE TABLE `dia1`.`marks` (`mark_id` INT NOT NULL AUTO_INCREMENT,`studen` INT NOT NULL,`subject_id` INT NOT NULL,`mark` INT NOT NULL,PRIMARY KEY (`mark_id`))";
+
+// connection.query(sql, function(err,result){
+//     if(err){
+//         console.log(err);
+//     }else{
+//         console.log("Tabla creada");
+//         console.log(result);
+//     }
+// });
+
+
+// sql = "SELECT first_name,last_name FROM dia1.teachers;";
+
+// connection.query(sql, function(err,result){
+//     if(err){
+//         console.log(err);
+//     }else{
+//         console.log("Tabla creada");
+//         console.log(result);
+//     }
+// });
+
+// sql = "SELECT * FROM dia1.teachers;";
+
+// connection.query(sql, function(err,result){
+//     if(err){
+//         console.log(err);
+//     }else{
+//         console.log("Tabla creada");
+//         console.log(result);
+//     }
+// });
+
+
+sql = "DELETE FROM marks where date < '2012-01-01'";
+connection.query(sql,function(err,result){
     if(err){
         console.log(err);
     }else{
-        console.log("Tabla creada");
+        console.log("Información actualizada");
         console.log(result);
     }
-});
+})
 
-sql = "CREATE TABLE `dia1`.`subjects` (`subject_id` INT NOT NULL AUTO_INCREMENT,`title` VARCHAR(45) NULL,PRIMARY KEY (`subject_id`))";
-
-connection.query(sql, function(err,result){
+sql = "UPDATE marks SET mark=5 WHERE mark<5";
+connection.query(sql,function(err,result){
     if(err){
         console.log(err);
     }else{
-        console.log("Tabla creada");
+        console.log("Información actualizada");
         console.log(result);
     }
-});
+})
 
-sql = "CREATE TABLE `dia1`.`teachers` (`teacher_id` INT NOT NULL AUTO_INCREMENT,`first_name` VARCHAR(45) NULL,`last_name` VARCHAR(45) NULL,PRIMARY KEY (`teacher_id`))";
-
-connection.query(sql, function(err,result){
-    if(err){
-        console.log(err);
-    }else{
-        console.log("Tabla creada");
-        console.log(result);
-    }
-});
-
-sql = "CREATE TABLE `dia1`.`subject_teacher` (`subject_id` INT NOT NULL AUTO_INCREMENT,`teacher_id` INT NOT NULL,`group_id` INT NOT NULL,PRIMARY KEY (`subject_id`))";
-
-connection.query(sql, function(err,result){
-    if(err){
-        console.log(err);
-    }else{
-        console.log("Tabla creada");
-        console.log(result);
-    }
-});
-
-sql = "CREATE TABLE `dia1`.`groups` (`group_id` INT NOT NULL AUTO_INCREMENT,`name` INT NOT NULL,PRIMARY KEY (`group_id`))";
-
-connection.query(sql, function(err,result){
-    if(err){
-        console.log(err);
-    }else{
-        console.log("Tabla creada");
-        console.log(result);
-    }
-});
-
-sql = "CREATE TABLE `dia1`.`marks` (`mark_id` INT NOT NULL AUTO_INCREMENT,`studen` INT NOT NULL,`subject_id` INT NOT NULL,`mark` INT NOT NULL,PRIMARY KEY (`mark_id`))";
-
-connection.query(sql, function(err,result){
-    if(err){
-        console.log(err);
-    }else{
-        console.log("Tabla creada");
-        console.log(result);
-    }
-});
-
-
-sql = "SELECT first_name,last_name FROM dia1.teachers;";
-
-connection.query(sql, function(err,result){
-    if(err){
-        console.log(err);
-    }else{
-        console.log("Tabla creada");
-        console.log(result);
-    }
-});
-
-sql = "SELECT * FROM dia1.teachers;";
-
-connection.query(sql, function(err,result){
-    if(err){
-        console.log(err);
-    }else{
-        console.log("Tabla creada");
-        console.log(result);
-    }
-});
 
 connection.end();
